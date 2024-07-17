19162d98a3a364ed433623aec23c914089c0c0e7
Source61 <lillekalle@protonmail.com>
2024-07-17 04:11:40 +0200

    Probably last commit; there's lots of minor bugs/inconsistencies etc but those are no fun working on, tried PyTorch, Torchlib for GPU, single and multithreaded CPU and optimized all maximally(?), so all done for now

--- pytorch.pyx
+++ pytorch.pyx
@@ -6,32 +6,32 @@ dtype = torch.double
 from libc.math cimport ceil
 
 cdef:
-	# Config
-	unsigned long long memSize = int(1E8)*3
-	unsigned long long totalSize = int(1E10)*16
+  # Config
+  unsigned long long memSize = int(1E8)*3
+  unsigned long long totalSize = int(1E10)*16
 	
-	# Don't touch
-	unsigned long long i
-	double pi = 0
-	bint shouldSwitch = memSize % 2 == 1
-	bint switch = True
-	bint indexStart
+  # Don't touch
+  unsigned long long i
+  double pi = 0
+  bint shouldSwitch = memSize % 2 == 1
+  bint switch = True
+  bint indexStart
 
 start = time.time()
 for i in range(<unsigned long long>ceil(totalSize/memSize)):
-	print(i, i*memSize, (i+1)*memSize)
-	t1 = torch.ones(memSize, dtype=dtype, device=dev)
-	t2 = torch.arange(i*memSize, (i+1)*memSize, dtype=dtype, device=dev).mul(2).add(1)
-	if shouldSwitch:
-		if switch:
-			t2[1::2] = 0 - t2[1::2]
-		else:
-			t2[0::2] = 0 - t2[0::2]
-		switch = not switch
-	else:
-		t2[1::2] = 0 - t2[1::2]
-	t3 = torch.sum(t1.div(t2))
-	pi += t3.item()*4
+  print(i, i*memSize, (i+1)*memSize)
+  t1 = torch.ones(memSize, dtype=dtype, device=dev)
+  t2 = torch.arange(i*memSize, (i+1)*memSize, dtype=dtype, device=dev).mul(2).add(1)
+  if shouldSwitch:
+    if switch:
+      t2[1::2] = 0 - t2[1::2]
+    else:
+      t2[0::2] = 0 - t2[0::2]
+    switch = not switch
+  else:
+    t2[1::2] = 0 - t2[1::2]
+  t3 = torch.sum(t1.div(t2))
+  pi += t3.item()*4
 stop = time.time()
 
 print("Execution time: %fs" % ((stop-start,)))
--- torch/test.cpp
+++ torch/test.cpp
@@ -4,73 +4,48 @@
 #include <math.h>
 #include <torch/torch.h>
 
-// UNFINISHED
-
-torch::TensorOptions options = torch::TensorOptions().dtype(torch::kDouble).device(torch::kCUDA, 0);
+torch::TensorOptions options = torch::TensorOptions().dtype(torch::kDouble);
 
 double microtime() {
   return (double(std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count()) / double(1000000));
 }
 
-void oldTestCode() {
-  bool lastAddition = false;
-	std::vector<double> v;
-	v.reserve(1E9);
-  //v.resize(1E9);
-	for(unsigned long long i=0; i<1E9; i++) {
-		if (lastAddition)
-			v[i] = -i; // 0.7s
-		else
-			v[i] = i;
-	}
-  printf("v.capacity(): %d\n", v.capacity());
-	double start = microtime();
-	torch::Tensor tensor = torch::from_blob((double*)(v.data()), v.capacity());
-	torch::Tensor res = torch::sum(tensor);
-	double stop = microtime();
-	std::cout << res << std::endl;
-	printf("%f\n", stop-start);
-}
-
-void newTestCode() {
-  unsigned int memSize = 1E8;
-  unsigned long long totalSize = 1E8 * 16;
-
-  unsigned long long rounds = ceil(totalSize / memSize);
-  unsigned long long i;
-  bool lastAddition = false;
-  double* v1 = (double*)calloc(1, sizeof(double));
-  double* v2 = (double*)calloc(1, sizeof(double));
-  double local_pi = 0.0;
-
-  //torch::Tensor t1 = torch::from_blob((double*)(v1), 1).clone().to(torch::kCUDA);
-  //torch::Tensor t2 = torch::from_blob((double*)(v2), 1).clone().to(torch::kCUDA);
-  torch::Tensor t1 = torch::ones({memSize}, options);
-  torch::Tensor t2 = torch::empty({memSize}, options);
-  torch::Tensor t3 = torch::empty({memSize}, options);
-  double start = microtime();
-	for(i=0; i<rounds; i++) {
-    t1 = torch::ones({memSize}, options);
-    t2 = torch::arange(memSize, options).mul(2.0).add(1);
-    // Todo: make t2[::2] = 0 - t2[::2]
-    t3 = torch::sum(t1.div(t2));
-    //t1.add(t2);
-    //t1[0] = t2[0] = 1.0;
-    //torch::add(t1, t2);
-    // double value = 1.0 / (i * 2.0 + 1.0);
-		//if (lastAddition)
-    //  tensor[0] = (double)-i;
-		//else
-    //  tensor[0] = (double)i;
-	}
-  double stop = microtime();
-  printf("Execution time: %fs. Rounds: %d.\n", stop-start, i);
-  std::cout << "T1: " << t1[10].item() << ", T2: " << t2[10].item() << ", T3: " << t3.item() << std::endl;
-  free(v1);
-  free(v2);
-}
-
 int main() {
-  //oldTestCode();
-  newTestCode();
+  double tstart = microtime();
+  uint_fast64_t memSize = 1E8;
+  uint_fast64_t totalSize = 1E8 * 16;
+
+  uint_fast64_t rounds = ceil(totalSize / memSize);
+  uint_fast64_t i;
+  bool lastAddition = false;
+  double local_pi = 0.0;
+  bool shouldSwitch = memSize % 2 == 1;
+	bool theSwitch = true;
+  uint_fast8_t startIndex;
+  double pi = 0.0;
+
+  if (torch::cuda::is_available())
+    options = options.device(torch::kCUDA, 0);
+
+  torch::Tensor t1 = torch::ones({memSize}, options);
+  torch::Tensor t2;
+  torch::Tensor t3;
+  for(i=0; i<rounds; i++) {
+    t2 = torch::arange((i*memSize)+1, ((i+2)*memSize)+1, 2, options);
+    if (shouldSwitch) {
+      if (theSwitch)
+        startIndex = 1;
+      else
+        startIndex = 0;
+      theSwitch = !theSwitch;
+    }
+    else
+      startIndex = 1;
+    t2.index({torch::indexing::Slice(startIndex, torch::indexing::None, 2)}) = torch::negative(t2.index({torch::indexing::Slice(startIndex, torch::indexing::None, 2)}));
+    t3 = torch::sum(t1.div(t2));
+    pi += (t3*4).item<double>();
+	}
+  double tstop = microtime();
+  std::cout << "t2[-1]: " << t2[-1] << std::endl;
+  printf("Execution time: %fs. Samples: %llu (%llu rounds, memSize %llu). Pi: %.40f\n", tstop-tstart, totalSize, i, memSize, pi);
 }
